60ca6b2eec4166f18bfed7c50c14d0f2088a37b4
Giusepth Leonardo Rivera Ayala <giusepthrivera@gmail.com>
2025-05-23 15:10:19 -0500

    Subiendo versión actual del proyecto

---
+++ PsicologiaEscolar.API/Data/UsuarioRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using PsicologiaEscolar.API.Models;
+namespace PsicologiaEscolar.API.Data
+{ // Con esta clase buscamos utilizar los procedimientos almacenados que tenemos en la base de datos para la tabla usuario, el cual es para validar si el usuario existe
+    public class UsuarioRepository
+    {
+    }
+}
